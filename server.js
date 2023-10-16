352445ad791bf521cec3d9dbd974ff9d260bfa9a
Aleenaaroy <aleenaaroy@gmail.com>
2023-10-16 06:29:26 +0530

    issue fixed-back to home page from login even after session

--- server.js
+++ server.js
@@ -1,67 +1,85 @@
-const express = require('express');
-const session = require('express-session');
-const bodyParser = require('body-parser');
+const express = require("express");
+const session = require("express-session");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 3001;
 
-app.set('view engine','ejs');
-app.set('views','public');
-
-
+app.set("view engine", "ejs");
+app.set("views", "public");
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(session({ secret: '2Dasaleenasecretkey', resave: false, saveUninitialized: true }));
+app.use(
+  session({
+    secret: "2Dasaleenasecretkey",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
 // Predefined username and password for validation
-const validUsername = 'admin';
-const validPassword = 'password123';
+const validUsername = "aleena123";
+const validPassword = "password123";
+
+//Append data on login page "Incorrect username or password" or "successfully logged out"
 let data;
 
-
-
-app.get('/', (req, res) => {
-    data="";
-    res.render('login',{data:data});
+// Middleware to check if the user is authenticated
+const isAuthenticated = (req, res, next) => {
+  console.log(req.session);
+  if (req.session.isAuthenticated) {
+    next();
+  } else {
+    //Destroy session here after checking 'isAuthenticated' property
+    req.session.destroy((err) => {
+      if (err) {
+        console.error(err);
+      }
+      next("route");
+    });
+  }
+};
+// Login route
+app.get("/", isAuthenticated, (req, res) => {
+  res.render("index");
 });
 
-// Login route
-app.post('/login', (req, res) => {
-    const { username, password } = req.body;
-    req.session.user=username;
-    if (username === validUsername && password === validPassword) {
-        req.session.authenticated = true;
-        res.render('index');
-    } else {
-        data="Incorrect username or password";
-        res.render('login',{data:data});
-    }
+app.get("/", (req, res) => {
+  data = "";
+  res.render("login", { data: data });
+});
+
+//After login
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  if (username === validUsername && password === validPassword) {
+    req.session.isAuthenticated = true;
+
+    res.redirect("/"); 
+  } else {
+    data = "Incorrect username or password";
+    res.render("login", { data: data });
+  }
 });
 
 // Home route (protected)
-app.get('/home', (req, res) => {
-    if (req.session.authenticated) {
-        res.render('index');
-    } else {
-        data="";
-        res.render('login',{data:data});
-    }
-});
+// app.get("/home", isAuthenticated, (req, res) => {
+//   if (req.session.isAuthenticated) {
+//     res.render("index");
+//   } else {
+//     data = "";
+//     res.render("login", { data: data });
+//   }
+// });
 
 // Logout route
-app.get('/logout', (req, res) =>{
-    data="successfully logged out";
-    req.session.user=null;
-    req.session.save(err=>{
-        if(err)
-     console.log(err); 
-    });  
-    res.render('login',{data:data});
+app.get("/logout", (req, res) => {
+  //data = "successfully logged out";
+  req.session.isAuthenticated = false;
+  res.redirect("/"); 
 });
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
-
-
